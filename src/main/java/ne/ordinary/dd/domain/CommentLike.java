ee92d503f6580931524ea4e103761b9752a5152b
rlJzr <eopy0928@naver.com>
2023-11-25 20:30:37 +0900

    feat : comment, commentLike domain 생성

--- src/main/java/ne/ordinary/dd/domain/CommentLike.java
+++ src/main/java/ne/ordinary/dd/domain/CommentLike.java
@@ -1,4 +1,20 @@
 package ne.ordinary.dd.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
 public class CommentLike {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //댓글 공감 식별자
+
+    private Long commentId; //댓글 식별자
+    private Long type; //공감 유형 1이면 F, 2면 T
+    private Long userId; //공감한 유저 식별자
+
 }
