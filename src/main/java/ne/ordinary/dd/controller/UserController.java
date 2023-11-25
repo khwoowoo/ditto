fbcd7fd7d6641d0918c4e46963ce1b572202f4c1
aeeazip <codnjs0809@naver.com>
2023-11-26 04:30:20 +0900

    feat: chatGPT로 MBTI TEST 하기 구현

--- src/main/java/ne/ordinary/dd/controller/UserController.java
+++ src/main/java/ne/ordinary/dd/controller/UserController.java
@@ -8,6 +8,7 @@ import ne.ordinary.dd.model.ResponseDTO;
 import ne.ordinary.dd.model.UserRequestDto;
 import ne.ordinary.dd.model.UserResponseDto;
 import ne.ordinary.dd.repository.UserRepository;
+import ne.ordinary.dd.service.ChatGptService;
 import ne.ordinary.dd.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,22 @@ public class UserController {
 
     public final UserService userService;
     public final UserRepository userRepository;
+    public final ChatGptService chatGptService;
     @PostMapping("/user/register")
     public ResponseDTO<Long> register(@RequestBody UserRequestDto userRequestDto){
+
+        int score = chatGptService.createMBTITest("Q: 나 오늘 택배 뜯다가 칼에 손 베였어\n" +
+                "A: 괜찮아? 어떡해? 아팠겠다. 병원은 갔어?\n" +
+                "\n" +
+                "Q: 너 오늘 힘들어 보이길래 이거 보낸다. 힘내!\n" +
+                "A: 고마워 잘 먹을게!(내가 힘들어보였나?)\n" +
+                "\n" +
+                "Q: 하..나 오늘 헤어졌어..위로 좀 해줘\n" +
+                "A: 괜찮아?? 너무 힘들겠다 기다려봐 내가 전화할게\n" +
+                "\n" +
+                "Q: 나 늦을 것 같아. 퇴근 시간이라서 막히네\n" +
+                "A: 음 알겠어 기다리고 있을게!(근데 사과는 없네)");
+        System.out.println(score);
 
         if(userRequestDto.getUuid() == null){
             throw new Exception500("user register error");
--- src/main/java/ne/ordinary/dd/service/ChatGptService.java
+++ src/main/java/ne/ordinary/dd/service/ChatGptService.java
@@ -1,39 +1,38 @@
-//package ne.ordinary.dd.service;
-//
-//import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-//import com.theokanning.openai.completion.chat.ChatMessage;
-//import com.theokanning.openai.service.OpenAiService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//
-//import java.time.Duration;
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class ChatGptService {
-//    private static final String MODEL = "gpt-3.5-turbo";
-//    private OpenAiService openAiService;
-//
-//    @Value("${chatgpt.key}")
-//    private String apiKey;
-//
-//    // ChatGPT Mbti Test
-//    public int createGptComment(String content) {
-//        this.openAiService = new OpenAiService(apiKey, Duration.ofSeconds(60));
-//        String prompt = "내가 작성한 다이어리를 한 줄 단위로 감정분석한 결과는 다음과 같다.\n"
-//                + content
-//                + "모든 문장의 postive와 negative 값을 분석해서 positive 80 이상이면 happy, positive 60 이상이면 smile, positive 40 이상이면 not bad, negative 60 이상이면 sad, negative 80 이상이면 upset을 의미한다.\n"
-//                + "오늘 하루 내 감정이 어떤 것에 가까운지 알려줘.\n"
-//                + "단 어떠한 설명도 없이 happy, smile, not bad, sad, upset 중 한 단어로만 알려줘.";
-//
-//        ChatCompletionRequest requester = ChatCompletionRequest.builder()
-//                .model(MODEL)
-//                .maxTokens(2048)
-//                .messages(List.of(
-//                        new ChatMessage("user", prompt)
-//                )).build();
-//        return Integer.parseInt(openAiService.createChatCompletion(requester).getChoices().get(0).getMessage().getContent());
-//    }
-//}
+package ne.ordinary.dd.service;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatGptService {
+    private static final String MODEL = "gpt-3.5-turbo";
+    private OpenAiService openAiService;
+
+    @Value("${chatgpt.key}")
+    private String apiKey;
+
+    // ChatGPT Mbti Test
+    public int createMBTITest(String content) {
+        this.openAiService = new OpenAiService(apiKey, Duration.ofSeconds(60));
+        String prompt = "너는 지금부터 MBTI 검사 판독기야.\n" +
+                "\n" + content +
+                "너는 4가지 질문을 보고 질문의 평균값을 구해서 100점 만점에 내가 몇 점짜리 F형 인간인지  알려줘.\n" +
+                "무조건 점수로만 말해줘. 다른 부연설명이나 분석없이 무조건 숫자로만 대답해줘. (예. 80)";
+
+        ChatCompletionRequest requester = ChatCompletionRequest.builder()
+                .model(MODEL)
+                .maxTokens(2048)
+                .messages(List.of(
+                        new ChatMessage("user", prompt)
+                )).build();
+        return Integer.parseInt(openAiService.createChatCompletion(requester).getChoices().get(0).getMessage().getContent());
+    }
+}
